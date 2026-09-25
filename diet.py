0ca4b5dbc2c8b57bce98423de727b82d673b3db7
luxmi-11y <luxmijaiswal9971@gmail.com>
2026-09-25 11:03:28 +0530

    Add files via upload

---
+++ diet.py
@@ -0,0 +1,40 @@
+def bmi_calculator(weight,height):
+  bmi=weight/((height/100)**2)
+  return round(bmi,2)
+
+
+def bmr_calculator(gender, age ,weight,height):
+    if gender =="Male":
+       bmr=(10*weight)+(6.25*height)-(5*age)+5
+       return bmr
+    elif gender=="Female":
+        bmr=(10*weight)+(6.25*height)-(5*age)+161
+        return bmr 
+
+def tdee_calculator(bmr,activity):
+      activity_factor={"Sedentary":1.20,
+                       "Lightly active":1.375,
+                       "Moderately active ":1.55,
+                       "Very active":1.725,
+                       "Extra active":1.90
+                 }
+      tdee=bmr*activity_factor[activity]
+      return  round(tdee,2)
+
+def calorie_target(tdee,aim):
+    if aim=="weight maintaine":
+        calorie=tdee
+    elif aim=="weight loss":
+        calorie=tdee-400
+    elif aim=="weight gain":
+         calorie=tdee+300
+    else:
+        calorie=tdee   
+    return round(calorie,2)
+
+# print(bmi_calculator(60,150))
+# bmr=bmr_calculator("Male",25,50,150)
+# tdee=tdee_calculator(bmr,"Very active")
+
+# print(calorie_target(tdee,"weight loss"))
+
